9049d684c1100ac88151cae6cb4966d8950217e1
MishaPatel <mishapatel0311@gmail.com>
2026-07-16 14:41:24 -0400

    Update api-gateway issue

--- backend/api-gateway/src/main/java/com/blocksign/gateway/config/SecurityConfig.java
+++ backend/api-gateway/src/main/java/com/blocksign/gateway/config/SecurityConfig.java
@@ -5,37 +5,41 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
   @Bean
-public SecurityWebFilterChain filterChain(
-    ServerHttpSecurity http) {
-  return http
-    .csrf(csrf -> csrf.disable())
-    .cors(cors -> cors.configurationSource(
-        corsConfigurationSource()))
-    .authorizeExchange(ex -> ex.anyExchange().permitAll())
-    .build();
-}
+  public SecurityWebFilterChain filterChain(
+      ServerHttpSecurity http) {
+    return http
+        .csrf(csrf -> csrf.disable())
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        .authorizeExchange(ex -> ex.anyExchange().permitAll())
+        .build();
+  }
 
-@Bean
-public CorsConfigurationSource corsConfigurationSource() {
-  CorsConfiguration config = new CorsConfiguration();
-  config.setAllowedOrigins(List.of(
-      "http://localhost:4200",
-      "https://https://blocksign-1qilt7wdn-mishapatel.vercel.app"
-  ));
-  config.setAllowedMethods(List.of(
-      "GET", "POST", "PUT", "DELETE", "OPTIONS"));
-  config.setAllowedHeaders(List.of("*"));
-  config.setAllowCredentials(true);
-  
-  UrlBasedCorsConfigurationSource source = 
-      new UrlBasedCorsConfigurationSource();
-  source.registerCorsConfiguration("/**", config);
-  return source;
-}
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of(
+        "http://localhost:4200",
+        "https://blocksign-1qilt7wdn-mishapatel.vercel.app"
+    ));
+    config.setAllowedMethods(List.of(
+        "GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source =
+        new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+    return source;
+  }
 }
